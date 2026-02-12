39382e667d7a67883643cae7d7e0b080be3b975c
Aaron Kyle Velasquez <avelasquez4@gitlab.com>
2026-02-12 23:49:12 +0800

    AgentResponse

--- main.py
+++ main.py
@@ -2,36 +2,56 @@ from dotenv import load_dotenv
 from tavily import TavilyClient
 from langchain_core.prompts import PromptTemplate
 from langchain_ollama import ChatOllama
+from langchain_openai import ChatOpenAI
 from langchain_tavily import TavilySearch
 from langchain.agents import create_agent
 from langchain_core.messages import HumanMessage
 from langchain_core.tools import tool
+from typing import List
+from pydantic import BaseModel, Field
 
 load_dotenv()
+# tavily = TavilyClient()
 
-tavily = TavilyClient()
+# @tool
+# def search(query:str) -> str:
+#     """
+#     Tool that searches over internet
+#     Args:
+#         query: The query to search for
+#     Returns:
+#         The search results as a string
+#     """
+#     print(f"Searching for: {query}")
+#     # return "Tokyo weather is sunny"
+#     return tavily.search(query=query)
+    
+class Source(BaseModel):
+    """Schema for a source used by the the agent"""
+    url:str = Field(description="The URL of the source")
 
-@tool
-def search(query:str) -> str:
-    """
-    Tool that searches over internet
-    Args:
-        query: The query to search for
-    Returns:
-        The search results as a string
-    """
-    print(f"Searching for: {query}")
-    # return "Tokyo weather is sunny"
-    return tavily.search(query=query)
+class AgentResponse(BaseModel):
+    """Schema for agent response with answer and sources"""
+    answer:str = Field(description="The agent's answer to the query")
+    sources: List[Source] = Field(default_factory=list, description="List of sources used to generate the answer")
 
-llm = ChatOllama(model="llama3.2")
+# llm = ChatOllama(model="llama3.2")
+llm = ChatOllama(model="mistral")
+
+# llm = ChatOpenAI()
 # tools = [search]
 tools = [TavilySearch()]
-agent = create_agent(model=llm, tools=tools)
+agent = create_agent(model=llm, tools=tools, response_format=AgentResponse)
 
 def main():
     # result = agent.invoke({"messages": [HumanMessage(content="What is the weather in Tokyo?")]})
-    result = agent.invoke({"messages": [HumanMessage(content="search for 1 job posting for an ai engineer using langchain in the bay area on linkedin and list their details")]})
+    result = agent.invoke(
+        {
+            "messages": HumanMessage(
+                content="search for 1 job posting for an ai engineer using langchain in the bay area on linkedin and list their details"
+            )
+        }
+    )
     print(result)
 
     # print("Hello from lagnchain-course!")
