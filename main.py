da75e83f1602d3890137734d98a9c80aaa17f79f
Aaron Kyle Velasquez <avelasquez4@gitlab.com>
2026-02-20 22:38:49 +0800

    agent_executor

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+#deprecated
+
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -10,10 +12,24 @@ from langchain_tavily import TavilySearch
 
 
 tools = [TavilySearch()]
+llm = ChatOllama(model="qwen3:1.7b")
+react_prompt = hub.pull("hwchase17/react")
+agent = create_react_agent( # reasoning agent
+    llm=llm,
+    tools=tools,
+    prompt=react_prompt,
+)
 
+agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=True) # agent runtime - call a tool or run another LLM call
+chain = agent_executor
 
-def main() -> None:
-    print("Hello from langchain-course")
+def main():
+    # print("Hello from langchain-course")
+    result = chain.invoke(
+        input={
+            "input": "search for 1 job posting for an ai engineer using langchain in the bay area on linkedin and list their details"
+        }
+    )
 
 
 if __name__ == "__main__":
